3fced9168530710d70b73bbfba13cfb33c75a5ca
Evgeny Tretyakov <ztretyakov@gmail.com>
2020-09-21 14:03:38 +0700

    Day 1 tasks are done, so ez

--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
-
+        for (int a = 1980; a <= 2020 ; a += 4) {
+            System.out.println("Олимпиада " + a + " года");
+            // если использовать решение System.out.printf("Олимпиада %d года\n", a);,
+            // то тест ругается, что "org.opentest4j.AssertionFailedError: Не верное количество строк со словом JAVA".
+            // решение работает, - тест на меня почему быканул? :D
+        }
     }
 }
